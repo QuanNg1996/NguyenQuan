2a6c826872415681759eb711ead92eb04da9afd3
Quan Nguyen <quanguyen250@gmail.com>
2020-01-23 18:42:07 -0500

    JavaScript smooth scroll effect

--- app.js
+++ app.js
@@ -3,5 +3,28 @@
       $(this).toggleClass('open');
       $('.top-nav').toggleClass('open');
     });
+
+    $('.top-nav .nav-link').on('click', function () {
+      $('.menu-toggler').removeClass('open');
+      $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+      $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+      },2000);
+    });
+
+    $('#button').on('click', function () {
+      $('html, body').animate({
+        scrollTop: 575
+      }, 2000);
+    });
+
+    $('#up').on('click', function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 2000)
+    });
 });
 
