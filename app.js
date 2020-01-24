6d210df71c14fc6201278f886d8a085d9c0e4f4f
Quan Nguyen <quanguyen250@gmail.com>
2020-01-23 20:17:50 -0500

    AOS animation

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-  $(document).ready(function () {
-    $('.menu-toggler').on('click', function () {
-      $(this).toggleClass('open');
-      $('.top-nav').toggleClass('open');
-    });
+$(document).ready(function () {
+  $('.menu-toggler').on('click', function () {
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
+  });
 
-    $('.top-nav .nav-link').on('click', function () {
-      $('.menu-toggler').removeClass('open');
-      $('.top-nav').removeClass('open');
-    });
+  $('.top-nav .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
 
-    $('nav a[href*="#"]').on('click', function () {
-      $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top -270
-      },2000);
-    });
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top -270
+    },2000);
+  });
 
-    $('#button').on('click', function () {
-      $('html, body').animate({
-        scrollTop: 575
-      }, 2000);
-    });
+  $('#button').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 575
+    }, 2000);
+  });
 
-    $('#up').on('click', function() {
-      $('html, body').animate({
-        scrollTop: 0
-      }, 2000)
-    });
+  $('#up').on('click', function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 2000)
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1800
+  });
 });
