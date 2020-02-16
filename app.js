64dd020a5375202bc380310d05d03685f90d6bbf
Quan Nguyen <quanguyen250@gmail.com>
2020-02-16 15:54:10 -0500

    clean

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 $(document).ready(() => {
-  $('.menu-toggler').on('click', function () {
+  $('.menu-toggler').on('click', function() {
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
   });
 
-  $('.top-nav .nav-link').on('click', function () {
+  $('.top-nav .nav-link').on('click', function() {
     $('.menu-toggler').removeClass('open');
     $('.top-nav').removeClass('open');
   });
 
-  $('nav a[href*="#"]').on('click', function () {
+  $('nav a[href*="#"]').on('click', function() {
     $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top -270
     }, 2000);
   });
 
-  $('#button').on('click', function () {
+  $('#button').on('click', function() {
     $('html, body').animate({
       scrollTop: 575
     }, 2000)
